a7b473816ea644a2cf022910e1070080f09fb155
mohsenrs <mohsen8022@gmail.com>
2021-05-27 18:16:44 +0430

    1

---
+++ src/views/EventList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+import EventService from "../services/EventService";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then((res) => (this.events = res.data))
+      .catch((err) => console.log(err.message));
+  },
+};
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
